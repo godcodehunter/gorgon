5abc33a940ee36e6a73aa94a9e994cb3c5b2f169
Dmitry Opokin <mamhigtt@gmail.com>
2023-10-07 23:47:18 +0700

    Changes

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use std::{
     sync::Arc,
 };
 use syntax::{
-    ast::{FieldList, HasAttrs, HasDocComments, HasModuleItem, HasName, HasVisibility, Item, Enum},
-    SourceFile, AstNode,
+    ast::{
+        BlockExpr, Enum, Expr, FieldList, HasAttrs, HasDocComments, HasModuleItem, HasName,
+        HasVisibility, IfExpr, Item, Stmt, LetStmt, Pat, Type,
+    },
+    AstNode, SourceFile,
 };
 
 async fn parse_file(graph: &mut Graph, path: &Path) -> anyhow::Result<()> {
@@ -38,21 +41,215 @@ async fn parse_file(graph: &mut Graph, path: &Path) -> anyhow::Result<()> {
         ));
     }
 
+    println!("{}", q_body);
+    graph.execute(query(&q_body)).await.unwrap();
+
     for item in tree.items() {
-        let items = match item {
-            Item::Const(i) => {
-                todo!()
-            }
-            Item::Enum(item) => gen_enum(graph, item).await,
-            Item::Module(i) => {}
-            _ => {}
-        };
+        gen_item(&graph, item);
     }
 
     Ok(())
 }
 
-async fn gen_enum(graph: &Graph, item: Enum) {
+async fn gen_item(graph: &Graph, item: Item) -> String {
+    let id = match item {
+        Item::Const(item) => {
+            // Constant { name }
+            // HAS_TYPE
+            // HAS_ATTR
+            // VISIBLE_FROM
+            // TODO: Expr
+            // TODO: default for specialization
+
+            let expr = item.body().unwrap();
+        }
+        Item::Enum(item) => gen_enum(graph, item).await,
+        Item::Module(i) => todo!(),
+        _ => todo!(),
+    };
+
+    "TODO_SOME_ID".to_string()
+}
+
+async fn gen_expr(graph: &Graph, item: Expr) -> String {
+    return match item {
+        syntax::ast::Expr::IfExpr(item) => gen_if_expr(graph, item).await,
+        _ => todo!(),
+    };
+}
+
+async fn gen_if_expr(graph: &Graph, item: IfExpr) -> String {
+    let mut q_body = String::new();
+
+    q_body.push_str(&format!("CREATE (if_expr:IfExpr) RETURN ID(if_expr)\n"));
+
+    for attr in item.attrs() {
+        let meta = attr.meta().unwrap();
+        q_body.push_str(&format!(
+            "CREATE (if_expr)<-[:HAS_ATTR]-(:Attr {{meta: \"{}\" }})\n",
+            meta
+        ));
+    }
+
+    let id = gen_expr(graph, item.condition().unwrap()).await;
+    q_body.push_str(&format!("CREATE (if_expr)<-HAS_CONDITION-(TODO)\n"));
+    if let Some(expr) = item.else_branch() {
+        let node = match expr {
+            syntax::ast::ElseBranch::Block(expr) => gen_block(graph, expr).await,
+            syntax::ast::ElseBranch::IfExpr(expr) => gen_if_expr(graph, expr).await,
+        };
+
+        q_body.push_str(&format!("CREATE (if_expr)<-HAS_ELSE-(TODO)\n"));
+    }
+
+    graph.execute(query(&q_body)).await.unwrap();
+    return "TODO_SOME_ID".to_string();
+}
+
+async fn gen_block(graph: &Graph, expr: BlockExpr) -> String {
+    let mut q_body = String::new();
+
+    q_body.push_str(&format!("CREATE (block: BlockExpression)\n"));
+
+    for attr in expr.attrs() {
+        q_body.push_str(&format!(
+            "CREATE (block)<-[:HAS_ATTR]-(:Attr {{meta: \"{}\" }})\n",
+            attr
+        ));
+    }
+  
+    if let Some(stms) = expr.stmt_list() {
+        for stmt in stms.statements() {
+            let id = match stmt {
+                Stmt::ExprStmt(expr) => gen_expr(graph, expr.expr().unwrap()).await,
+                Stmt::Item(item) => gen_item(graph, item).await,
+                Stmt::LetStmt(let_stmt) => gen_let_stmt(graph, let_stmt).await,
+            };
+        }
+    }
+
+    return "TODO_SOME_ID".to_string();
+}
+
+///
+/// let_stmt <--[HAS_ATTR]-- attr
+///   ^  ^
+///   |  |--[HAS_PATTERN]-- pattern <--[HAS_TYPE]-- type
+///   |                       ^
+///   |                       |--[HAS_INITIALIZER] -- initializer
+///   |----[HAS_ELSE]-- block
+/// 
+async fn gen_let_stmt(graph: &Graph, let_stmt: LetStmt) -> String {
+    let mut q_body = String::new();
+    //TODO: resolve pattern (pattern_piece)-POINT_TO->(some_declaration_field)
+    
+    q_body.push_str(&format!("CREATE (let_stmt: LetStatement) RETURN ID(let_stmt)\n"));
+    for attr in let_stmt.attrs() {
+        let meta = attr.meta().unwrap();
+        q_body.push_str(&format!(
+            "CREATE (let_stmt)<-[:HAS_ATTR]-(:Attr {{meta: \"{}\" }})\n",
+            meta
+        ));
+    }
+    let pat_id = gen_pattern(graph, let_stmt.pat().unwrap()).await;
+    q_body.push_str(&format!("CREATE (let_stmt)<-HAS_PATTERN-(pat: TODO)\n"));
+
+    if let Some(ty) = let_stmt.ty() {
+        let ty_id = gen_type(graph, ty).await;
+        q_body.push_str(&format!("CREATE (pat)<-HAS_TYPE-(pat: TODO)\n"));
+    }
+    
+    let initializer = let_stmt.initializer().unwrap();
+    let init_id = gen_expr(graph, initializer).await;
+    q_body.push_str(&format!("CREATE (pat)<-HAS_INITIALIZER-(TODO)\n"));
+
+    let else_expr = let_stmt.let_else().unwrap().block_expr().unwrap();
+    let else_id = gen_block(graph, else_expr).await;
+    q_body.push_str(&format!("CREATE (let_stmt)<-HAS_ELSE-(TODO)\n"));
+
+    println!("{}", q_body);
+    graph.execute(query(&q_body)).await.unwrap();
+
+    return "TODO_SOME_ID".to_string();
+}
+
+async fn gen_pattern(graph: &Graph, pat: Pat) -> String {
+    match pat {
+        //
+        // pattern { name, has_ref, has_mut } <--[HAS_ATTR]-- attr
+        //   ^
+        //   |---[HAS_PATTERN] -- pattern 
+        // 
+        Pat::IdentPat(item) => {
+            let mut q_body = String::new();
+
+            let has_ref = if item.ref_token().is_some() {
+                ", has_ref: true"
+            } else {
+                ""
+            };
+
+            let has_mut = if item.mut_token().is_some() {
+                ", has_mut: true"
+            } else {
+                ""
+            };
+
+            q_body.push_str(
+                &format!("CREATE (pat: IdentifierPattern {{ name: \"{}\" {} {} }})\n", 
+                item.name().unwrap(),
+                has_ref,
+                has_mut
+            ));
+
+            for attr in item.attrs() {
+                let meta = attr.meta().unwrap();
+                q_body.push_str(&format!(
+                    "CREATE (pat)<-[:HAS_ATTR]-(:Attr {{meta: \"{}\" }})\n",
+                    meta
+                ));
+            }   
+
+            if let Some(pat) = item.pat() {
+                let pat_id = gen_pattern(graph, pat).await;
+                q_body.push_str(&format!("CREATE (pat)<-HAS_PATTERN-(pat: TODO)\n"));
+            }
+
+            println!("{}", q_body);
+            graph.execute(query(&q_body)).await.unwrap();
+        },
+        //
+        // pattern
+        //  ^ ^ ^ ...
+        //  | | |
+        //  | |  ----[HAS_PATTERN] -- pattern 
+        //  | -------[HAS_PATTERN] -- pattern 
+        //  ---------[HAS_PATTERN] -- pattern 
+        //
+        Pat::OrPat(pat) => {
+            let mut q_body = String::new();
+
+            q_body.push_str(
+                &format!("CREATE (pat: OrPattern )\n"
+            ));
+
+            for pat in pat.pats() {
+                let pat_id = gen_pattern(graph, pat).await;
+                
+                q_body.push_str(&format!("CREATE (pat)<-HAS_PATTERN-(pat: TODO)\n"));
+            }
+        }
+        _ => todo!(),
+    }
+
+    todo!()
+}
+
+async fn gen_type(graph: &Graph, ty: Type) -> String {
+    todo!()
+}
+
+async fn gen_enum(graph: &Graph, item: Enum) -> String {
     //TODO: generic params ???
     //TODO: where clause ???
     //TODO: visibility - what is target node???
@@ -162,6 +359,8 @@ async fn gen_enum(graph: &Graph, item: Enum) {
     }
     println!("{}", q_body);
     graph.execute(query(&q_body)).await.unwrap();
+
+    return "TODO_SOME_ID".to_string();
 }
 
 #[tokio::main]
@@ -201,6 +400,7 @@ async fn main() {
     let parse_result = SourceFile::parse(payload).ok().unwrap();
     let enums = parse_result.syntax().descendants().filter_map(Enum::cast);
     for item in enums {
-        gen_enum(&graph, item);
+        let id = gen_enum(&graph, item).await;
+        dbg!(id);
     }
 }
